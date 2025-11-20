3143d9c34d90cb334fda5adc88a0d977b19ca51b
LizzieLiu01 <liu_ym0212@163.com>
2025-11-20 10:11:07 +0800

    cpp_win

---
+++ cpp_win/main.cpp
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+//#include <cuda_provider_factory.h>
+#include <onnxruntime_cxx_api.h>
+#include"text_det.h"
+#include"text_angle_cls.h"
+#include"text_rec.h"
+
+using namespace cv;
+using namespace std;
+using namespace Ort;
+
+
+int main()
+{
+	TextDetector detect_model;
+	TextClassifier angle_model;
+	TextRecognizer rec_model;
+
+	// string imgpath = "/Users/zhangxin/github/PaddleOCR-v3-onnxrun-cpp-py/cpp/images/1.jpg";
+	string imgpath = R"(D:\github\PaddleOCR-v3-onnxrun-cpp-py\cpp_win\images\1.jpg")";
+	cout << imgpath <<endl;
+	Mat srcimg = imread(imgpath);
+	///cv::rotate(srcimg, srcimg, 1);
+	cout << "image: " << imgpath << ", img.shape:" << srcimg.rows << "," << srcimg.cols << endl;
+
+	vector< vector<Point2f> > results = detect_model.detect(srcimg);
+	cout << "size of boxes : " << results.size() << endl;
+	for (size_t i = 0; i < results.size(); i++)
+	{
+		Mat textimg = detect_model.get_rotate_crop_image(srcimg, results[i]);
+		/*imshow("textimg", textimg);
+		waitKey(0);
+		destroyAllWindows();*/
+		if (angle_model.predict(textimg) == 1)
+		{
+			cv::rotate(textimg, textimg, 1);
+		}
+		string text = rec_model.predict_text(textimg);
+		cout << text << endl;
+	}
+	detect_model.draw_pred(srcimg, results);
+	
+	static const string kWinName = "Deep learning object detection in ONNXRuntime";
+	namedWindow(kWinName, WINDOW_NORMAL);
+	imshow(kWinName, srcimg);
+	waitKey(0);
+	destroyAllWindows();
+	//imwrite("result.jpg", srcimg);
+}
